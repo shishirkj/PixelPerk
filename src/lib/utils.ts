83d8bed41a51fdc26b8b739278c000a579de684b
Shishir <skjskjskj333@gmail.com>
2024-02-24 15:48:30 +0530

    clerk webhooks added for user

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { MongooseError } from "mongoose";
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -15,7 +17,12 @@ export const handleError = (error: unknown) => {
     // This is a string error message
     console.error(error);
     throw new Error(`Error: ${error}`);
-  } else {
+  } 
+  else if(error instanceof MongooseError)
+  { //mongoose error
+    throw new Error(error.message)
+  }
+  else {
     // This is an unknown type of error
     console.error(error);
     throw new Error(`Unknown error: ${JSON.stringify(error)}`);
